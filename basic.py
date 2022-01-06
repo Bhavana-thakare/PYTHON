1d1ff3c0dd8eb412f8c1a3a2e62fcb0bb9777e9b
bhavana_thakare_ <91379054+Bhavana-thakare@users.noreply.github.com>
2022-01-06 23:27:15 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,105 @@
+# a="nav"
+# b="gurukul"
+# c="123"
+# print(a+b+c)
+
+# a=10
+# b="5"
+# print(a+5,str(b))
+
+# a="amit"
+# b="19.6"
+# c=18
+# b=float(b)
+# print(a*2+"b")
+
+# number_x = ("75")
+# number_y =("45")
+# number_x = int(number_x)
+# number_y = int(number_y) 
+# print (number_x * number_y)
+
+# 123_abc="19.1"
+# print(float(123_abc))
+# print(int(123_abc))
+
+# a="pooja"
+# b="neha"
+# c="chaya"
+# b="priya"
+# print(a+b+c+b)
+
+# a="10.0"
+# print (float(a))
+
+# str3="'hello'"""""""+"""world"""
+# print(str3)
+
+# a=5
+# a=a*"5"
+# print(a)
+# print(type(a))
+
+# number=int(input("enter the number"))
+# print(number and 1)
+
+# var1="nav"
+# var2="gurukul"
+# print(type(var1+var2))
+
+# a=10
+# b=20
+# c=30
+# d=40
+# e=25
+# print(a//b**c+b-d*e%a)
+
+# Name="bhavana"
+# print(Name[0])
+# print(Name[1])
+# print(Name[2])
+# print(Name[3])
+# print(Name[4])
+# print(Name[5])
+# print(Name[6])
+# print(Name[::-1])
+# print(len(Name))
+
+
+# Name="bhavana"
+# print(Name[::-1])
+
+# a="bhavana\n"
+# b="pratik"
+# print(a+b)
+
+# a="12 hours"
+# print(12*2,"hours")
+
+# a="19.0"
+# b=19
+# print(a+b)
+
+# a=5
+# b=9
+# print(a!=b)
+
+# a=19
+# b=5
+# print(not(a<=b or a==b))
+
+# a=2
+# b=5
+# print(a is b)
+
+# a="420"
+# print("2" in a)
+
+
+# a='in'or'not'
+# print(a)
+
+# a=2+4j
+# b=4+6j
+# print(a*b)
+
---
+++ docs.py
@@ -0,0 +1,118 @@
+# A string with zero character is called Null string.
+
+# str1 = "Welcome to my blog"                   
+# str2 = "Welcome to my \n Blog"
+# print(str1)
+# print(str2)
+
+# str1 = "Welcome \tto my Blog"
+# str2 = "Welcome to\n my \tBlog"
+# print(str1)                
+# print(str2)
+
+# str1 = """ Welcome to my 
+#                 blog.
+#                 This is for
+#                 Class X"""
+# print(str1)
+
+# str="hello"
+# print(str[:3])
+
+# Python string concatenation is the process of merging two or more strings.
+
+
+# str1 = "Welcome to my Blog"
+# for i in str1:
+#     print(i)
+#     print(i, end =' ')
+#     print(i, end = '#')    
+
+
+# str1 = "Amit"
+# for i in str1:
+#     print(i)
+#     print(i, end =' ')
+
+# str="str1"+"str2"
+# print(str)
+
+# Sometimes we need to repeat the string in the program, and we can do this easily by using the 
+# repetition operator in Python.
+
+# s='My'                        
+# s1='Blog'                        
+# s2=s[:1]+s1[len(s1)-1:]                        
+# print(s2)
+# print("My" *3 + "Blog" +"7")
+
+# for i in range(2,7,2):                
+#     print(i * '2')
+
+# for i in range(3,12, 2):                
+#     print("a".upper())
+
+# for i in range(3,12,13):               
+#     print("a".upper)
+
+# s='Welcome to My Site https://nayaksworld.com '              
+# print(s.find('come'))              
+# print(s.find('o'))
+# print(s.find('o', 10, 20))
+# print(s.find('o', 5, 10))
+
+# txt = "Hello, welcome to my world."
+# x = txt.find("welcome")
+# print(x) 
+
+        
+# for i in range(2,5,2):
+#     print(i * '@')
+
+# a='h' in 'Hindi'
+# b='i' in 'Hindi'
+# print(a,b)
+
+# for i in "Amit":
+#     print(i in "Amit")
+
+
+# a="string" in "substring"
+# b="string" not in "substring"
+# print(a,b)
+
+# a='tie' == 'ties'
+# b="Amit" != "Amitabh"
+# c="amit" > "Amitabh"
+# print(c)
+
+# a = 10
+# b = 4
+# print("a & b =", a & b)
+# print("a | b =", a | b)
+# print("~a =", ~a)
+# print("a ^ b =", a ^ b)
+
+# a = 10
+# b = -10
+# print("a >> 1 =", a >> 1)
+# print("b >> 1 =", b >> 1)
+# a = 5
+# b = -10
+# print("a << 1 =", a << 1)
+# print("b << 1 =", b << 1)
+
+# l=[1, 0, 2, 0, 'hello', '', []]
+# print(list(filter(bool, l)))
+
+# print([] or {})
+# print({} or [])
+
+# if (9 < 0) and (0 < -9):
+#     print("hello")
+# elif (9 > 0) or False:
+#     print("good")
+# else:
+#     print("bad")
+
+
